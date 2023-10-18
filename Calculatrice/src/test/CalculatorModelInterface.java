796a38a119a56912c3548f317d65bf000eb77721
plpat <plpat@LAPTOP-K7KNESMG>
2023-10-18 14:30:14 +0200

    remplissage classe calculatormodelinterface

--- Calculatrice/src/test/CalculatorModelInterface.java
+++ Calculatrice/src/test/CalculatorModelInterface.java
@@ -1,5 +1,15 @@
 package test;
 
-public class CalculatorModelInterface {
-
+public interface CalculatorModelInterface {
+	public void add();
+	public void substract();
+	public void multiply();
+	public void divide();
+	public void opposite();
+	public void push();
+	public void pop();
+	public void drop();
+	public void swap();
+	public void clear();
+	
 }
