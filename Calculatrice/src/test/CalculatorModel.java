bf3a06b0759ee84e2d6b4ea62b594c9b5d66bb33
Mérélys <preira.merelys@gmail.com>
2023-10-23 22:57:29 +0200

    classe main

--- Calculatrice/src/test/CalculatorModel.java
+++ Calculatrice/src/test/CalculatorModel.java
@@ -1,5 +1,6 @@
 package test;
-
+import java.util.Stack; 
 public class CalculatorModel {
+
 
 }
--- Calculatrice/src/test/CalculatorModelInterface.java
+++ Calculatrice/src/test/CalculatorModelInterface.java
@@ -1,5 +1,17 @@
 package test;
+import java.util.Stack;
+public class CalculatorModelInterface{
+	
+private Stack<Double> pile = new Stack<>();
 
-public class CalculatorModelInterface {
+public void push(double value) {
+	pile.push(value);
+}
 
+public double pop() {
+	if (pile.isEmpty()){
+		return 0;
+	}
+	return pile.pop();
+}
 }
